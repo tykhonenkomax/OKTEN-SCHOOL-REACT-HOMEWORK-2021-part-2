d6c9c6a3d8e96fbe6df7f14f6f3188eb8003f687
Max <tykhonenko.work@gmail.com>
2022-10-05 13:17:06 +0300

    reducer in component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import {useState} from "react";
-
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
 
 function App() {
 
+  const state = useSelector(state => state.userReducer);
+  console.log(state)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(value => value.json())
+        .then(value => dispatch({type: 'GET-USERS', payload: value}))
+
+  }, [])
+
   return (
       <div>
-App
+        app
       </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,94 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import {Provider} from "react-redux";
+import {combineReducers, createStore} from "redux";
+
+const initialUserReducerState = {users: [], user: null}
+
+const userReducer = (state = initialUserReducerState, action) => {
+  switch (action.type) {
+
+    case 'GET-USERS':
+      return {
+        ...state,
+        users: action.payload
+      };
+
+    case 'CHOOSE_USER':
+      let id = action.payload
+      let user = state.users.find(value => value.id === id)
+      return {
+        ...state,
+        user: user
+      }
+
+    default:
+      return state;
+
+  }}
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const initialPostReducerState = {posts: [], post: null}
+
+  const postReducer = (state = initialPostReducerState, action) => {
+    switch (action.type) {
+
+      case 'GET-POSTS':
+        return {
+          ...state,
+          users: action.payload
+        };
+
+      case 'CHOOSE_POST':
+        let id = action.payload
+        let post = state.posts.find(value => value.id === id)
+        return {
+          ...state,
+          post: post
+        }
+
+      default:
+        return state;
+
+    }}
+
+    const initialCommentReducerState = {comments: [], comment: null}
+
+    const commentReducer = (state = initialCommentReducerState, action) => {
+      switch (action.type) {
+
+        case 'GET-COMMENTS':
+          return {
+            ...state,
+            comments: action.payload
+          };
+
+        case 'CHOOSE_COMMENT':
+          let id = action.payload
+          let comment = state.comments.find(value => value.id === id)
+          return {
+            ...state,
+            comment: comment
+          }
+
+        default:
+          return state;
+
+      }}
+
+      const reducers = combineReducers({
+        userReducer,
+        postReducer,
+        commentReducer
+      });
+      
+  let store = createStore(reducers);
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+      <Provider store={store}>
+        <App/>
+      </Provider>
+  );
 
